a44b0711258d708d88c52aa74bb848b80944ad2b
PeterVasilopoulos <peterjvasilopoulos@gmail.com>
2024-02-02 12:24:36 -0500

    starting to use props

--- src/index.js
+++ src/index.js
@@ -73,12 +73,36 @@ function Menu() {
     return (
         <main className='menu'>
             <h2>Our Menu</h2>
-            <Pizza />
-            <Pizza />
-            <Pizza />
-            <Pizza />
+            <Pizza 
+              name='Pizza Spinaci' 
+              ingredients='Tomato, mozarella, spinach, and ricotta cheese' 
+              photoName='pizzas/spinaci.jpg'
+              price={10}
+            />
+
+            <Pizza 
+              name='Pizza Funghi'
+              ingredients='Tomato, mushrooms'
+              price={12}
+              photoName='pizzas/funghi.jpg'
+            />
         </main>
     )
+}
+
+function Pizza(props) {
+  console.log(props)
+
+  return (
+      <div className='pizza'>
+          <img src={props.photoName} alt={props.name} />
+          <div>
+            <h3>{props.name}</h3>
+            <p>{props.ingredients}</p>
+            <span>{props.price + 3}</span>
+          </div>
+      </div>
+  )
 }
 
 function Footer() {
@@ -101,16 +125,6 @@ function Footer() {
     )
 
     // return React.createElement('footer', null, "We're currently open!")
-}
-
-function Pizza() {
-    return (
-        <div>
-            <img src='pizzas/spinaci.jpg' alt='pizza spinaci' />
-            <h3>Pizza Spinaci</h3>
-            <p>Tomato, mozarella, spinach, and ricotta cheese</p>
-        </div>
-    )
 }
 
 // React v18
