95c00824ae7ed6a0e6b91d923cd8eef63c2e9a2e
PeterVasilopoulos <peterjvasilopoulos@gmail.com>
2024-02-05 11:22:02 -0500

    added a react fragment

--- src/index.js
+++ src/index.js
@@ -71,17 +71,27 @@ function Header() {
 
 function Menu() {
   const pizzas = pizzaData
+  // const pizzas = []
   const numPizzas = pizzas.length;
 
     return (
       <main className='menu'>
         <h2>Our Menu</h2>
 
+
         {/* Conditional Rendering Using Short Circuiting */}
         {numPizzas > 0 ? (
-          <ul className='pizzas'>
-            {pizzas.map(pizza =><Pizza pizzaObj={pizza} key={pizza.name} />)}
-          </ul>
+          // React Fragment
+          <>
+            <p>
+              Authentic Italian cuisine. 6 creative dishes to choose from.
+              All from our stone oven, all organic, all delicious.
+            </p>
+
+            <ul className='pizzas'>
+              {pizzas.map(pizza =><Pizza pizzaObj={pizza} key={pizza.name} />)}
+            </ul>
+          </>
         ) : <p>W're still working on our menu. Please come back later :)</p>}
 
 
