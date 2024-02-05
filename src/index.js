58ef1aa5b8de35a4bacfd7f2d680d0f09f2d017f
PeterVasilopoulos <peterjvasilopoulos@gmail.com>
2024-02-05 11:00:23 -0500

    replaced short circuiting with ternary operators

--- src/index.js
+++ src/index.js
@@ -78,12 +78,13 @@ function Menu() {
         <h2>Our Menu</h2>
 
         {/* Conditional Rendering Using Short Circuiting */}
-        {numPizzas > 0 && <ul className='pizzas'>
-                {pizzas.map(pizza =><Pizza pizzaObj={pizza} key={pizza.name} />)}
-              </ul>
-        }
+        {numPizzas > 0 ? (
+          <ul className='pizzas'>
+            {pizzas.map(pizza =><Pizza pizzaObj={pizza} key={pizza.name} />)}
+          </ul>
+        ) : <p>W're still working on our menu. Please come back later :)</p>}
 
-            
+
 
             {/* <Pizza 
               name='Pizza Spinaci' 
@@ -119,7 +120,7 @@ function Pizza(props) {
 
 function Footer() {
     const hour = new Date().getHours()
-    const openHour = 10
+    const openHour = 12
     const closeHour = 22
     const isOpen = hour >= openHour && hour <= closeHour
     console.log(isOpen)
@@ -133,12 +134,12 @@ function Footer() {
     return (
       <footer className='footer'>
         {
-        isOpen && 
+        isOpen ? ( 
         <div className='order'>
           <p>We're open until {closeHour}:00. Come visit us or order online.</p>
           <button className='btn'>Order</button>
         </div>
-        }
+        ) : <p>We're happy to welcome you between {openHour}:00 and {closeHour}:00.</p>}
       </footer>
     )
 
